dea2b3fe020325172ed8799ae5f2e2529bf9ab70
dejohansson <davjom-5@student.ltu.se>
2019-12-12 17:12:45 +0100

    Day 12: Complete!

--- day12/day12.py
+++ day12/day12.py
@@ -1,19 +1,19 @@
-"""moons = [[[1, 4, 4], [0, 0, 0]],
+moons = [[[1, 4, 4], [0, 0, 0]],
         [[-4, -1, 19], [0, 0, 0]],
         [[-15, -14, 12], [0, 0, 0]],
-        [[-17, 1, 10], [0, 0, 0]],]"""
+        [[-17, 1, 10], [0, 0, 0]],]
 
-moons = [[[-8, -10, 0], [0, 0, 0]],
+"""moons = [[[-8, -10, 0], [0, 0, 0]],
         [[5, 5, 10], [0, 0, 0]],
         [[2, -7, 3], [0, 0, 0]],
-        [[9, -8, -3], [0, 0, 0]],]
+        [[9, -8, -3], [0, 0, 0]],]"""
 
 """moons = [[[-1,0,2], [0, 0, 0]],
         [[2,-10,-7], [0, 0, 0]],
         [[4,-8,8], [0, 0, 0]],
         [[3,5,-1], [0, 0, 0]],]"""
 
-"""for s in range(1000):
+for s in range(1000):
     for i, moon in enumerate(moons):
         for moon2 in moons[i:]:
             if moon[0][0] > moon2[0][0]:
@@ -52,48 +52,39 @@ for moon in moons:
         kin += abs(v)
     print(pot, kin)
     energy += pot*kin
-print(energy)"""
+print(energy)
+
+def gcd(x, y):
+   while(y):
+       x, y = y, x % y
+   return x
+
+def lcm(x, y):
+   lcm = (x*y)//gcd(x,y)
+   return lcm
 
 points = []
-pt = tuple([ (tuple(moon[0]), tuple(moon[1])) for moon in moons])
-points.append(pt)
-                #4686774924
-                #100000
-                #123400000     
-for num in range(10000000000):
-    if num%100000 == 0:
-        print(num)
+start = [tuple([(moon[0][i], moon[1][i]) for moon in moons]) for i in range(3)]
+time = [0,0,0]
 
-    pt = tuple([ (tuple(moon[0]), tuple(moon[1])) for moon in moons])
-    if pt in points and num != 0:
-        print(num)
-        break
-    
+for t in range(3):
+    num = 0
+    while True:
+        pt = tuple([(moon[0][t], moon[1][t]) for moon in moons])
+        if num != 0 and pt == start[t]:
+            time[t] = num
+            break
+            
+        num += 1
+        for i, moon in enumerate(moons):
+            for moon2 in moons[i:]:
+                if moon[0][t] > moon2[0][t]:
+                    moon[1][t] -= 1
+                    moon2[1][t] += 1
+                elif moon[0][t] < moon2[0][t]:
+                    moon[1][t] += 1
+                    moon2[1][t] -= 1
+        for moon in moons:
+            moon[0][t] += moon[1][t]
 
-    for i, moon in enumerate(moons):
-        for moon2 in moons[i:]:
-            if moon[0][0] > moon2[0][0]:
-                moon[1][0] -= 1
-                moon2[1][0] += 1
-            elif moon[0][0] < moon2[0][0]:
-                moon[1][0] += 1
-                moon2[1][0] -= 1
-
-            if moon[0][1] > moon2[0][1]:
-                moon[1][1] -= 1
-                moon2[1][1] += 1
-            elif moon[0][1] < moon2[0][1]:
-                moon[1][1] += 1
-                moon2[1][1] -= 1
-
-            if moon[0][2] > moon2[0][2]:
-                moon[1][2] -= 1
-                moon2[1][2] += 1
-            elif moon[0][2] < moon2[0][2]:
-                moon[1][2] += 1
-                moon2[1][2] -= 1
-        
-    for moon in moons:
-        moon[0][0] += moon[1][0]
-        moon[0][1] += moon[1][1]
-        moon[0][2] += moon[1][2]
+print(lcm(time[0], lcm(time[1], time[2])))
